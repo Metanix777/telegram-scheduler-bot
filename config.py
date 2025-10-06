07ea33163bac61753e0ae21b3230646b1750db2e
Metanix777 <47758762+Metanix777@users.noreply.github.com>
2025-10-06 16:50:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("8383759148:AAGJzPUVxiROwGtPI2daOAK_yXdHugAjKn0")
-ADMIN_IDS = [int(i) for i in os.getenv("@MultyNet", "").split(",") if i]
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_IDS = [int(i) for i in os.getenv("ADMIN_IDS", "").split(",") if i]
+
